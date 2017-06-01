770f12f3c730ccb3dd8bca87ec4fe7b0fec398b7
Patricia Hidalgo-Gonzalez <paty.hidalgo.g@gmail.com>
2017-06-01 15:18:34 -0700

    OLS for Jan

--- main.R
+++ main.R
@@ -99,9 +99,78 @@ lm(formula = net_load_ramp_t_MWh ~ weekday
    data = hourly_01_2016
    )
 
-#test
+# picking an arbitrary training set
+training_01_2016 <- subset(hourly_01_2016, day==4 | day==5 | day==6 | day==7 | day==8 | day==9 | day==10)
+test_set_01_2016 <- subset(hourly_01_2016, day!=4 & day!=5 & day!=6 & day!=7 & day!=8 & day!=9 & day!=10)
+
+# OLS without weekday variable
+ols_01_2016 <- lm(formula = net_load_ramp_t_MWh ~ net_load_MWh 
+   + net_load_ramp_t_minus_1_MWh
+   + net_load_ramp_t_minus_2_MWh
+   + net_load_ramp_t_minus_3_MWh
+   + MWh
+   + wind_solar_ramp_t_MWh
+   + wind_solar_ramp_t_minus_1_MWh
+   + wind_solar_ramp_t_minus_2_MWh
+   + wind_solar_ramp_t_minus_3_MWh,
+   data = training_01_2016
+)
+summary(ols_01_2016)
+
+# OLS less variables so they are more meaningful
+#ols_01_2016 <- lm(formula = net_load_ramp_t_MWh ~ net_load_ramp_t_minus_2_MWh
+#                  + wind_solar_ramp_t_minus_2_MWh
+#                  + wind_solar_ramp_t_minus_3_MWh,
+#                  data = training_01_2016)
+#summary(ols_01_2016)
+
+test_set_01_2016$ramp_prediction_01_2016 <-  (-3.267e+03 + (9.168e-02)*test_set_01_2016$net_load_MWh 
+                                            + (9.500e-02)*test_set_01_2016$net_load_ramp_t_minus_1_MWh
+                                            + (2.007e-01)*test_set_01_2016$net_load_ramp_t_minus_2_MWh
+                                            + (-7.366e-02)*test_set_01_2016$net_load_ramp_t_minus_3_MWh
+                                            + (-7.056e-02)*test_set_01_2016$MWh
+                                            + (-6.687e-01)*test_set_01_2016$wind_solar_ramp_t_MWh
+                                            + (4.085e-01)*test_set_01_2016$wind_solar_ramp_t_minus_1_MWh
+                                            + (-1.768e+00)*test_set_01_2016$wind_solar_ramp_t_minus_2_MWh
+                                            + (1.335e+00)*test_set_01_2016$wind_solar_ramp_t_minus_3_MWh)
+
+plot_this <- subset(test_set_01_2016, select=c('timestamp.x', 
+                                                      'net_load_ramp_t_MWh',
+                                                      'ramp_prediction_01_2016'))
+  
+write.csv(plot_this, "plot_this.csv")
+#############
+
+ggplot(plot_this, aes(x = plot_this$timestamp.x)) + 
+  geom_line(aes(y = plot_this$net_load_ramp_t_MWh), colour="blue") + 
+  geom_line(aes(y = plot_this$ramp_prediction_01_2016), colour = "grey") + 
+  ylab(label="Number of new members") + 
+  xlab("Week")  
+  
 
 
+#ugh!!
+plot_01_2016 <- ggplot(test_set_01_2016, aes(timestamp.x)) +
+  geom_line(aes(y = ramp_prediction_01_2016, colour = "red")) 
++ geom_line(aes(y = net_load_ramp_t_MWh, colour =  "blue"))
+
+#try 2
+library("reshape2")
+library("ggplot2")
+
+test_data_long <- melt(test_set_01_2016, id="timestamp.x")  # convert to long format
+
+ggplot(data=test_data_long,
+       aes(x=timestamp.x, y=value, colour=variable)) +
+  geom_line()
+
+# try 3
+
+ggplot(joinsByWeek, aes(x = week)) + 
+  geom_line(aes(y = rolling), colour="blue") + 
+  geom_line(aes(y = actual), colour = "grey") + 
+  ylab(label="Number of new members") + 
+  xlab("Week")
 
 
 
